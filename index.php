15664a66837c10fcca3c70328ec0867aa67af011
WnX <vandermeulen.christophe@gmail.com>
2022-03-18 13:26:44 +0100

    Hello World

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
 // connexion à la base
 require_once('connect.php');
-$req=$db->query ('select * from etudiant')or die;
+$req = $db->query('select * from etudiant') or die;
 $eleve = $_POST;
 
-
-$modifier= $_GET['modifier'] ?? null;
-if(isset($modifier)){
+//modifier
+$modifier = $_GET['modifier'] ?? null;
+if (isset($modifier)) {
 
     $query = $db->query('select * from etudiant where id=' . $modifier);
     $datamodif = $query->fetch(PDO::FETCH_ASSOC);
 }
 
+//supprimer
+$supprimer= $_GET['supprimer'] ?? null;
+if(isset($supprimer)){
+    $query = $db-> query ('delete from etudiant where id='.$supprimer.'');
+    header('location:index.php');
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,15 +30,16 @@ if(isset($modifier)){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logiciel Ecole</title>
 </head>
+
 <body>
-<H2>Logiciel Ecole</H2>
-<form action="sauvegarde.php<?= $url ?>" method="POST">
-    <fieldset>
-    <?php if($modifier===null) : ?>
-        <legend>Ajout</legend>
-    <?php else : ?>
-        <legend>Modifier</legend>
-    <?php endif ?>
+    <H2>Logiciel Ecole</H2>
+    <form action="sauvegarde.php<?= $url ?>" method="POST">
+        <fieldset>
+            <?php if ($modifier === null) : ?>
+                <legend>Ajout</legend>
+            <?php else : ?>
+                <legend>Modifier</legend>
+            <?php endif ?>
             <input type="hidden" name="id" value="<?= $datamodif['id'] ?? '' ?>"><br>
             <label>Nom : </label> <input type="text" name="nom" value="<?= $datamodif['nom'] ?? '' ?>"><br>
             <label>Prenom : </label> <input type="text" name="prenom" value="<?= $datamodif['prenom'] ?? '' ?>"><br>
@@ -42,38 +50,37 @@ if(isset($modifier)){
     </form>
 
     <a onclick="return confirm('Voulez vous vraiment supprimer tous les éléments?')" href="?reset"><button class="btn btn-primary">Vider la table</button></a>
-      <?php
-      if (isset($_GET['reset'])) {
-          $db-> query ('DELETE FROM etudiant');
-          header('location:index.php');
-      }
-      ?>
+    <?php
+    if (isset($_GET['reset'])) {
+        $db->query('DELETE FROM etudiant');
+        header('location:index.php');
+    }
+    ?>
 
-<?php
+    <?php
 
-// on affiche le tableau
-echo '<table border="1" >';
+    // on affiche le tableau
+    echo '<table border="1" >';
 
-echo '<tr>
+    echo '<tr>
 <td>ID</td>
 <td>Nom</td>
 <td>Prenom</td>
 <td>Adresse</td>
 <td>Modifier</td>
-<td>Supprimer</td></tr>' ;
+<td>Supprimer</td></tr>';
 
-while ($donnees=$req->fetch () )
-{
-	echo '<td>' .$donnees['id'].'</td>' ;
-	echo '<td>' .$donnees['nom'].'</td>' ;
-	echo '<td>' .$donnees['prenom'].'</td>' ;
-	echo '<td>' .$donnees['adresse'].'</td>' ;
-    echo '<td><a href="index.php?modifier=' . $donnees['id'] . '">Modifier</a></td>';
-    echo '<td><a href="supprimer.php?supprimer=' . $donnees['id'] . '" >Supprimer</a></td></tr>';
+    while ($donnees = $req->fetch()) {
+        echo '<td>' . $donnees['id'] . '</td>';
+        echo '<td>' . $donnees['nom'] . '</td>';
+        echo '<td>' . $donnees['prenom'] . '</td>';
+        echo '<td>' . $donnees['adresse'] . '</td>';
+        echo '<td><a href="index.php?modifier=' . $donnees['id'] . '">Modifier</a></td>';
+        echo '<td><a href="index.php?supprimer=' . $donnees['id'] . '" >Supprimer</a></td></tr>';
+    }
 
-}
-
-echo '</table>'; 
-?>
+    echo '</table>';
+    ?>
 </body>
+
 </html>
--- supprimer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('connect.php');
-
-$supprimer= $_GET['supprimer'] ?? null;
-if(isset($supprimer)){
-    $query = $db-> query ('delete from etudiant where id='.$supprimer.'');
-    header('location:index.php');
-}
